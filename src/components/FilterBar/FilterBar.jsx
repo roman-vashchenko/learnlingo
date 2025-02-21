9a5b7e03750f810d180dcac16901814991aa2e80
Roman Vashchenko <roman1991dp@gmail.com>
2025-02-21 22:10:38 +0200

    update

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -2,8 +2,11 @@ import css from "./FilterBar.module.css";
 
 const FilterBar = () => {
   return (
-    <div>
-      <div className={css.fieldLanguages}>
+    <div className={css.wrapper}>
+      <div className={css.fieldFilter}>
+        <svg width={20} height={20}>
+          <use href="/public/img/icons.svg#icon-drop"></use>
+        </svg>
         <label htmlFor="languages">Languages</label>
         <select name="languages" id="languages">
           <option value="french">French</option>
@@ -11,6 +14,30 @@ const FilterBar = () => {
           <option value="german">German</option>
           <option value="ukrainian">Ukrainian</option>
           <option value="polish">Polish</option>
+        </select>
+      </div>
+      <div className={css.fieldFilter}>
+        <svg width={20} height={20}>
+          <use href="/public/img/icons.svg#icon-drop"></use>
+        </svg>
+        <label htmlFor="levelOfKnowledge">Level of knowledge</label>
+        <select name="levelOfKnowledge" id="levelOfKnowledge">
+          <option value="a1Beginner">A1 Beginner</option>
+          <option value="a2Elementary">A2 Elementary</option>
+          <option value="b1Intermediate">B1 Intermediate</option>
+          <option value="b2UpperIntermediate">B2 Upper-Intermediate</option>
+        </select>
+      </div>
+      <div style={{ width: 124 }} className={css.fieldFilter}>
+        <svg width={20} height={20}>
+          <use href="/public/img/icons.svg#icon-drop"></use>
+        </svg>
+        <label htmlFor="price">Price</label>
+        <select name="price" id="price">
+          <option value="10$">10 $</option>
+          <option value="20$">20 $</option>
+          <option value="30$">30 $</option>
+          <option value="40$">40 $</option>
         </select>
       </div>
     </div>
--- src/pages/Teachers/Teachers.jsx
+++ src/pages/Teachers/Teachers.jsx
@@ -1,10 +1,12 @@
 import FilterBar from "../../components/FilterBar/FilterBar";
+import TeachersList from "../../components/TeachersList/TeachersList";
 import css from "./Teachers.module.css";
 
 const Teachers = () => {
   return (
     <section className={css.section}>
       <FilterBar />
+      <TeachersList />
     </section>
   );
 };
