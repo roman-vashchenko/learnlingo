c530f13cbebcd214a28977036a2ea6f16ce12628
Roman Vashchenko <roman1991dp@gmail.com>
2025-03-01 23:46:36 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,24 +4,16 @@ import Logo from "../Logo/Logo";
 import NavBar from "../NavBar/NavBar";
 import UserBar from "../UserBar/UserBar";
 import css from "./AppBar.module.css";
-import { selectAuth, selectLoggedIn } from "../../redux/auth/selectors";
+import { selectLoggedIn } from "../../redux/auth/selectors";
 
 const AppBar = ({ openModal }) => {
   const isLoggedIn = useSelector(selectLoggedIn);
-  const { isRefreshing } = useSelector(selectAuth);
 
   return (
     <header className={css.header}>
       <Logo />
       <NavBar />
-      {isRefreshing ? (
-        <p>...loading</p>
-      ) : (
-        <div>
-          {isLoggedIn ? <UserBar /> : <AuthNav openModal={openModal} />}
-        </div>
-      )}
-      {/* {isLoggedIn ? <UserBar /> : <AuthNav openModal={openModal} />} */}
+      {isLoggedIn ? <UserBar /> : <AuthNav openModal={openModal} />}
     </header>
   );
 };
